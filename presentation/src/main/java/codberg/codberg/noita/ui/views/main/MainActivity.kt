b28f26e02a747e1d0075e76def7b248ee603eab6
min <min@codberg.com>
2022-02-21 09:27:22 +0900

    Initial commit

---
+++ presentation/src/main/java/codberg/codberg/noita/ui/views/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package codberg.codberg.noita.ui.views.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import codberg.codberg.noita.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
